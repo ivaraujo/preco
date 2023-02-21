de49da37fbab4452eaf0bfd5ea0df8f1efd95c9e
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2023-02-21 13:05:56 -0300

    Inserindo autocomplete input

---
+++ pages/validar_produtos.php
@@ -0,0 +1,23 @@
+<?php
+
+include("conexao.php");
+
+function retorna($barcode,$mysqli){
+    $validar_produtos = "SELECT * FROM mercadorias WHERE barcode = '$barcode' LIMIT 1";
+    $resultado_produtos = $mysqli->query($validar_produtos) or die("<p>Falha na execução do código SQL: </p>" . $mysqli->error);
+    if($resultado_produtos->num_rows){
+        $row_produtos = mysqli_fetch_assoc($resultado_produtos);
+        $valores['produto'] = $row_produtos['produto'];
+        $valores['marca'] = $row_produtos['marca'];
+        $valores['quantidade'] = $row_produtos['quantidade'];
+    }
+    else{
+        $valores['produto'] = 'ERRO!';
+    }
+    return json_encode($valores);
+}
+
+if(isset($_GET['barcode'],$mysqli)){
+    echo retorna();
+}
+?>
